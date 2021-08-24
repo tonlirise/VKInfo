76f29491416610f67e11d9472b231226305265a0
tonlirise <tonlirise>
2021-08-24 14:42:25 +0700

    Front elements init

--- app/src/main/java/com/example/vkinfo/MainActivity.java
+++ app/src/main/java/com/example/vkinfo/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.vkinfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText ed_search;
+    private Button btn_search;
+    private TextView tv_result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ed_search = findViewById(R.id.et_serch_text);
+        btn_search = findViewById(R.id.id_serarch_button);
+        tv_result = findViewById(R.id.tv_result);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv_result.setText(ed_search.getText());
+            }
+        };
+        btn_search.setOnClickListener(onClickListener);
     }
 }
